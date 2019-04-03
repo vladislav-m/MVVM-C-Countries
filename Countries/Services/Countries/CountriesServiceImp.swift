25d66904161e13e26833e1d318326d801ffeed51
Vladislav Myakotin <vladislav.myakotin@gmail.com>
2019-04-03 18:04:00 +0300

    Dummy implementations for services were added

---
+++ Countries/Services/Countries/CountriesServiceImp.swift
@@ -0,0 +1,9 @@
+//
+//  CountriesServiceImp.swift
+//  Countries
+//
+//  Created by Vladislav Myakotin on 03/04/2019.
+//  Copyright © 2019 Vladislav Myakotin. All rights reserved.
+//
+
+import Foundation
---
+++ Countries/Services/Country/CountryServiceImp.swift
@@ -0,0 +1,15 @@
+//
+//  CountryServiceImp.swift
+//  Countries
+//
+//  Created by Vladislav Myakotin on 03/04/2019.
+//  Copyright © 2019 Vladislav Myakotin. All rights reserved.
+//
+
+import RxSwift
+
+class CountryServiceImp: CountryService {
+    func fetchCountry(code: String) -> Single<Country> {
+
+    }
+}
