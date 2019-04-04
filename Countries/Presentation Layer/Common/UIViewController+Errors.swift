abe015f24ee52b9a703353b19c1b63f7cf83dc8d
Vladislav Myakotin <vladislav.myakotin@gmail.com>
2019-04-04 20:53:28 +0300

    UIViewController extension for errors displaing
    was implemented

---
+++ Countries/Presentation Layer/Common/UIViewController+Errors.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Errors.swift
+//  Countries
+//
+//  Created by Vladislav Myakotin on 04/04/2019.
+//  Copyright © 2019 Vladislav Myakotin. All rights reserved.
+//
+
+import Foundation
